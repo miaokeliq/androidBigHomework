3334c6f14c1e308e1ab45a15d794db6a0bcc7b7c
immkl <921418076@qq.com>
2019-11-03 15:48:28 +0800

    mkl Commit

---
+++ app/src/main/java/com/example/bighomework/Activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bighomework.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.bighomework.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
